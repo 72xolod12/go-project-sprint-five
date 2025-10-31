1ed374011813bd8afcc1592c5f577be6eaea3eb7
72xolod12 <ivashentsev.karaganda@gmail.com>
2025-10-31 09:27:39 +0500

    third-commit

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -63,10 +63,10 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
 	var calories float64
 	switch trainingType {
 
-	case "running":
+	case "running", "Бег":
 		calories, err = RunningSpentCalories(steps, weight, height, duration)
 
-	case "walking":
+	case "walking", "Ходьба":
 		calories, err = WalkingSpentCalories(steps, weight, height, duration)
 
 	default:
